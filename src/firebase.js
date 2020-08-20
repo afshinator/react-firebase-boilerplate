521e7bbfdae60a17b4ba3fe7c5738cdcb1245dac
Afshin Mokhtari <afshinator@hotmail.com>
2020-08-19 22:46:00 -0700

    Use difft fbase account for testing

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: "AIzaSyAEhUWdsEqDv9RghSgu38FkRNBnCjXFf20",
+  authDomain: "thinkpiece2.firebaseapp.com",
+  databaseURL: "https://thinkpiece2.firebaseio.com",
+  projectId: "thinkpiece2",
+  storageBucket: "thinkpiece2.appspot.com",
+  messagingSenderId: "739233581077",
+  appId: "1:739233581077:web:6d29bb1fd946b59417a61c"
+};
+
+firebase.initializeApp(config);
+
+window.firebase = firebase;   // for debug TODO:
+
+export default firebase;

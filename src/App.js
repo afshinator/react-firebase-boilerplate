e6b76f1ad9e1cbfff8e9c7fe76187f0ee35af014
Afshin Mokhtari <afshinator@hotmail.com>
2020-08-21 19:39:38 -0700

    Add ability to add and delete posts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-import { firestore } from './firebase'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { firestore } from "./firebase";
+import { collectIdsAndDocs } from './utils/misc';
+import Posts from "./components/Posts";
 
 function App() {
-  React.useEffect( ()=> {
-    const posts = firestore.collection('posts')
+  const [posts, setPosts] = React.useState([])
+
+  const handleCreate = async post => {
+    const docRef = await firestore.collection('posts').add(post)
+    const doc = await docRef.get()
+    const newPost = collectIdsAndDocs(doc)
+  
+    setPosts([newPost, ...posts]);
+  };
+
+  const handleRemove = async id => {
+    const allPosts = posts;
+    await firestore.doc(`posts/${id}`).delete()
+    const p = allPosts.filter(po => po.id !== id)
+    setPosts(p)
+  }
+
+  React.useEffect(() => {
+    firestore
+      .collection("posts")
       .get()
-      .then( (snap)=>{
-        snap.forEach(doc => {
-          const id = doc.id
-          const data = doc.data()
+      .then((snapshot) => {
+        const p = snapshot.docs.map(collectIdsAndDocs);
+        setPosts(p)
 
-          console.group({id, data})
-        })
-      })
-  }, [])
+      });
+  }, []);
 
-
+  console.log(posts)
   return (
     <div className="App">
       <header className="App-header">
@@ -35,6 +51,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Posts posts={posts} onCreate={handleCreate} onRemove={handleRemove}/>
     </div>
   );
 }
